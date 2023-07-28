c85b44d8e6e447106a1456d471ac718de52b47c5
AASStechclub2324 <140600729+AASStechclub2324@users.noreply.github.com>
2023-07-28 15:54:10 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
 
-cred = credentials.Certificate(r"C:\Users\USER\Desktop\Spam_recognition\database\unselphish-firebase-adminsdk-gmw7s-d6bc0bb794.json")
+cred = credentials.Certificate(r"")
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'unselphish.appspot.com'
 })
