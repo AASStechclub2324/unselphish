bd87557114ebfacfefbc577b77e02f2cf00cf0e9
AASStechclub2324 <140600729+AASStechclub2324@users.noreply.github.com>
2023-08-08 22:52:27 +0530

    Modularising main.py

--- main.py
+++ main.py
@@ -11,10 +11,133 @@ import model_exe
 import whatsapp_analysis
 import db
 
-try:
-    scantype = int(input('''1. Scan link (virustotal)\n2. Threat report from downloaded email (.eml)\n3. Scan singular message\n4. Threat report from exported whatsapp chat \n5. Scan file (virustotal)\n\nOption: '''))
-except:
-    sys.exit(1)
+def scan_link():
+    url2scan = str(input("Url: "))
+    url2scan = url2scan.split("?")[0]
+    url2scan = url2scan.split(" =")[0]
+    url2scan = url2scan.split("#")[0]
+    try:
+        sus_percent, malcount, suscount = vtl.scanlink(url2scan, verbosescan=True)
+        if sus_percent or malcount or suscount:
+            activescanbool = str(input("Active scan url (verbose)? (yes/no) default-yes: "))
+            if activescanbool.lower().strip() == "yes" or activescanbool.strip() == "":
+                verbosescanbool = str(input("Verbose (y/n) default n: "))
+                if "y" in verbosescanbool:
+                    vtl.active_scanlink(url2scan, verbosescan=True)
+                else:
+                    print("\nSCANNING URL. THIS MIGHT TAKE A MINUTE.")
+                    vtl.active_scanlink(url2scan, verbosescan=False)
+            else:
+                sys.exit(1)
+    except Exception as e:
+        printv(e)
+
+
+def eml_scan():
+    emlfile = str(input(".eml file: "))
+    email_subject, received_from_addr, received_from_ip, reply_to, emailtext, links_in_email = emlscan.parse_eml(emlfile)
+    index = complete_scan_text([email_subject + " " + emailtext], linklist=links_in_email)
+    sus_details = []
+    if index < 6:
+        filepath = r"C:\Users\Anutosh\Desktop\detail.txt"  # Change the file path to your needs
+        detail = {"Email Reports": {"Sender's Address": received_from_addr, "Sender's IP": received_from_ip, "threat index": index}}
+        sus_details.append(detail)
+
+
+    with open(filepath, 'w') as file:
+        file.write(str(sus_details))
+    db.update_storage(filepath)
+
+
+def single_scan():
+    ## Single Message Scan
+    msg2scan = str(input("Text message to scan: "))
+
+    ## Extracting links before replacing them
+    links_in_chatmessage = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',msg2scan)
+
+    #################################### DATA CLEANING ######################################
+
+    #CONVRTING EVERYTHING TO LOWERCASE
+    msg2scan=msg2scan.lower()
+
+    #REPLACING NEXT LINES BY 'WHITE SPACE'
+    msg2scan=msg2scan.replace(r'\n'," ") 
+
+    # REPLACING EMAIL IDs BY 'MAILID'
+    msg2scan=msg2scan.replace(r'^.+@[^\.].*\.[a-z]{2,}$','MailID')
+
+    # REPLACING URLs  BY 'Links'
+    msg2scan=msg2scan.replace(r'^http\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$','Links')
+
+    # REPLACING CURRENCY SIGNS BY 'MONEY'
+    msg2scan=msg2scan.replace(r'£|\$', 'Money')
+
+    # REPLACING LARGE WHITE SPACE BY SINGLE WHITE SPACE
+    msg2scan=msg2scan.replace(r'\s+', ' ')
+
+    # REPLACING LEADING AND TRAILING WHITE SPACE BY SINGLE WHITE SPACE
+    msg2scan=msg2scan.replace(r'^\s+|\s+?$', '')
+
+    #REPLACING CONTACT NUMBERS
+    msg2scan=msg2scan.replace(r'^\(?[\d]{3}\)?[\s-]?[\d]{3}[\s-]?[\d]{4}$','contact number')
+
+    #REPLACING SPECIAL CHARACTERS  BY WHITE SPACE 
+    msg2scan=msg2scan.replace(r"[^a-zA-Z0-9]+", " ")
+
+    #################################### DATA CLEANING END ######################################
+
+    index = complete_scan_text([msg2scan])
+
+
+
+def whatsapp_scan():
+    ## whatsapp threat report from txt file
+
+    ################ INITIAL FILTERING OF WHATSAPP MESSAGES #########################
+    chattxt = str(input("Filepath of whatsapp chat file(.txt): "))
+    parsedchat_authors, parsedchat_messages = whatsapp_analysis.parse_chat_file(chattxt)
+    sus_messages = []
+    chatblacklistedwordsfoundall = []
+    linksinchat_all = []
+    for message in parsedchat_messages:
+        chatblacklistedwords, chatblacklistedcount = blacklist.check_blacklisted_keywords(message)
+        if chatblacklistedcount > 0:
+            author = parsedchat_authors[list(parsedchat_messages).index(message)]
+            msg_detail = {"author": author, "msg": message}
+            sus_messages.append(msg_detail)
+            chatblacklistedwordsfoundall += chatblacklistedwords
+        links_in_chatmessage = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',message)
+        linksinchat_all += links_in_chatmessage
+    #### INITIAL FILTERING END #########################
+    print(sus_messages)
+    ## Database Update ##
+    details = []
+    auth = input("\nEnter message author to be scanned: ")
+    msg2scan = []
+
+    for sus in sus_messages:
+        if sus["author"] == auth:
+            spam_msg = sus["msg"]
+            msg2scan.append(spam_msg)
+
+    index = complete_scan_text(msg2scan, linksinchat_all)
+    if index < 6:
+        filepath = r"C:\Users\Anutosh\Desktop\detail.txt"  # Change the file path to your needs
+        detail = {"Whatsapp Reports": {'author': str(auth), 'threat index': index}}
+        details.append(detail)
+
+
+    with open(filepath, 'a') as file:
+        file.write(str(details))
+    # db.update_storage(filepath)
+    ## Database Update End ##
+    
+
+def file_scan():
+    fpath = str(input("Filepath of file to scan: "))
+    vt2.active_scan_file(fpath)
+
 
 def complete_scan_text(text_list=[], linklist=[]):
 
@@ -98,130 +221,29 @@ def complete_scan_text(text_list=[], linklist=[]):
 
     return index
 
-if scantype == 1:
-    url2scan = str(input("Url: "))
-    url2scan = url2scan.split("?")[0]
-    url2scan = url2scan.split(" =")[0]
-    url2scan = url2scan.split("#")[0]
+
+################# Executable for CLI tool #######################
+
+if __name__ == "__main__":
+
     try:
-        sus_percent, malcount, suscount = vtl.scanlink(url2scan, verbosescan=True)
-        if sus_percent or malcount or suscount:
-            activescanbool = str(input("Active scan url (verbose)? (yes/no) default-yes: "))
-            if activescanbool.lower().strip() == "yes" or activescanbool.strip() == "":
-                verbosescanbool = str(input("Verbose (y/n) default n: "))
-                if "y" in verbosescanbool:
-                    vtl.active_scanlink(url2scan, verbosescan=True)
-                else:
-                    print("\nSCANNING URL. THIS MIGHT TAKE A MINUTE.")
-                    vtl.active_scanlink(url2scan, verbosescan=False)
-            else:
-                sys.exit(1)
-    except Exception as e:
-        printv(e)
-    pass
+        scantype = int(input('''1. Scan link (virustotal)\n2. Threat report from downloaded email (.eml)\n3. Scan singular message\n4. Threat report from exported whatsapp chat \n5. Scan file (virustotal)\n\nOption: '''))
+    except:
+        sys.exit(1)
 
-if scantype == 2:
-    emlfile = str(input(".eml file: "))
-    email_subject, received_from_addr, received_from_ip, reply_to, emailtext, links_in_email = emlscan.parse_eml(emlfile)
-    index = complete_scan_text([email_subject + " " + emailtext], linklist=links_in_email)
-    sus_details = []
-    if index < 6:
-        filepath = r"C:\Users\Anutosh\Desktop\detail.txt"  # Change the file path to your needs
-        detail = {"Email Reports": {"Sender's Address": received_from_addr, "Sender's IP": received_from_ip, "threat index": index}}
-        sus_details.append(detail)
+    if scantype == 1:
+        scan_link()
 
+    if scantype == 2:
+        eml_scan()
+        
+    if scantype == 3:
+        single_scan()
 
-    with open(filepath, 'w') as file:
-        file.write(str(sus_details))
-    # db.update_storage(filepath)
-
-    
-if scantype == 3:
-    ## Single Message Scan
-    msg2scan = str(input("Text message to scan: "))
-
-    ## Extracting links before replacing them
-    links_in_chatmessage = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',msg2scan)
-
-    #################################### DATA CLEANING ######################################
-
-    #CONVRTING EVERYTHING TO LOWERCASE
-    msg2scan=msg2scan.lower()
-
-    #REPLACING NEXT LINES BY 'WHITE SPACE'
-    msg2scan=msg2scan.replace(r'\n'," ") 
-
-    # REPLACING EMAIL IDs BY 'MAILID'
-    msg2scan=msg2scan.replace(r'^.+@[^\.].*\.[a-z]{2,}$','MailID')
-
-    # REPLACING URLs  BY 'Links'
-    msg2scan=msg2scan.replace(r'^http\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$','Links')
-
-    # REPLACING CURRENCY SIGNS BY 'MONEY'
-    msg2scan=msg2scan.replace(r'£|\$', 'Money')
-
-    # REPLACING LARGE WHITE SPACE BY SINGLE WHITE SPACE
-    msg2scan=msg2scan.replace(r'\s+', ' ')
-
-    # REPLACING LEADING AND TRAILING WHITE SPACE BY SINGLE WHITE SPACE
-    msg2scan=msg2scan.replace(r'^\s+|\s+?$', '')
-
-    #REPLACING CONTACT NUMBERS
-    msg2scan=msg2scan.replace(r'^\(?[\d]{3}\)?[\s-]?[\d]{3}[\s-]?[\d]{4}$','contact number')
-
-    #REPLACING SPECIAL CHARACTERS  BY WHITE SPACE 
-    msg2scan=msg2scan.replace(r"[^a-zA-Z0-9]+", " ")
-
-    #################################### DATA CLEANING END ######################################
-
-    index = complete_scan_text([msg2scan])
-
-if scantype == 4:
-    ## whatsapp threat report from txt file
-
-    ################ INITIAL FILTERING OF WHATSAPP MESSAGES #########################
-    chattxt = str(input("Filepath of whatsapp chat file(.txt): "))
-    parsedchat_authors, parsedchat_messages = whatsapp_analysis.parse_chat_file(chattxt)
-    sus_messages = []
-    chatblacklistedwordsfoundall = []
-    linksinchat_all = []
-    for message in parsedchat_messages:
-        chatblacklistedwords, chatblacklistedcount = blacklist.check_blacklisted_keywords(message)
-        if chatblacklistedcount > 0:
-            author = parsedchat_authors[list(parsedchat_messages).index(message)]
-            msg_detail = {"author": author, "msg": message}
-            sus_messages.append(msg_detail)
-            chatblacklistedwordsfoundall += chatblacklistedwords
-        links_in_chatmessage = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',message)
-        linksinchat_all += links_in_chatmessage
-    #### INITIAL FILTERING END #########################
-    print(sus_messages)
-    ## Database Update ##
-    details = []
-    auth = input("\nEnter message author to be scanned: ")
-    msg2scan = []
-    print(parsedchat_authors)
-    for sus in sus_messages:
-        if sus["author"] == auth:
-            spam_msg = sus["msg"]
-            msg2scan.append(spam_msg)
-    print(msg2scan)
-
-    index = complete_scan_text(msg2scan, linksinchat_all)
-    if index < 6:
-        filepath = r"C:\Users\Anutosh\Desktop\detail.txt"  # Change the file path to your needs
-        detail = {"Whatsapp Reports": {'author': str(auth), 'threat index': index}}
-        details.append(detail)
-
-
-    with open(filepath, 'a') as file:
-        file.write(str(details))
-    # db.update_storage(filepath)
-    ## Database Update End ##
-    
+    if scantype == 4:
+        whatsapp_scan
 
 
 
-if scantype == 5:
-    fpath = str(input("Filepath of file to scan: "))
-    vt2.active_scan_file(fpath)
+    if scantype == 5:
+        file_scan()
